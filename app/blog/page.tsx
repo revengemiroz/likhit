a9cfcd024c79eec2469c8e4af233997a2660e7bb
aaakash06 <akashbagale182@gmail.com>
2024-10-24 23:45:09 +0545

    blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Driving License Guide",
+  description:
+    "Learn how to apply for a driving license, prepare for the test, and understand all the necessary steps to acquire your license. Get tips and insights to make the process smooth and successful.",
+  keywords:
+    "Driving License Guide, Apply for License, License Preparation, Driving Test Tips, How to Get a License, Driving License Requirements",
+  openGraph: {
+    images:
+      "https://utfs.io/f/Ug3TBysra1dXA9sImtE41SFw0mNZDItClxTshGraHEp72j4e", // Using the same image URL
+  },
+};
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
