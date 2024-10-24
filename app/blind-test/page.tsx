d3cceb50396ba25ec01d1baced27ffa710681310
aaakash06 <akashbagale182@gmail.com>
2024-10-24 23:26:56 +0545

    map and blindtest page meta done

--- app/blind-test/page.tsx
+++ app/blind-test/page.tsx
@@ -4,7 +4,18 @@ import React from "react";
 
 import Nav from "@/components/home/Nav";
 import Quiz from "@/app/blind-test/quiz";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Blind Test",
+  description:
+    "Participate in our exciting blind test quiz and challenge your knowledge!",
+  keywords:
+    "Blind Test, Quiz, Knowledge Challenge, Fun Quiz, Trivia, Test Your Knowledge",
+  openGraph: {
+    images:
+      "https://utfs.io/f/Ug3TBysra1dXA9sImtE41SFw0mNZDItClxTshGraHEp72j4e", // Using the same image URL
+  },
+};
 function page() {
   return (
     <div className=" min-h-screen flex gap-12 flex-col">
--- app/find-centers-near/page.tsx
+++ app/find-centers-near/page.tsx
@@ -1,7 +1,18 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 import { LoaderCircle } from "lucide-react";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Find Centers Near You",
+  description:
+    "Discover nearby centers with ease. Use our tool to locate the nearest centers for your needs, whether it's for health, education, or leisure. Get directions and contact information instantly.",
+  keywords:
+    "Find Centers Near Me, Nearby Centers, Local Centers, Center Locator, Health Centers Near Me, Education Centers Near Me, Leisure Centers Near Me, Directions to Centers, Center Contact Information",
+  openGraph: {
+    images:
+      "https://utfs.io/f/Ug3TBysra1dXA9sImtE41SFw0mNZDItClxTshGraHEp72j4e", // Replace with a relevant image URL
+  },
+};
 export default async function Page() {
   const Map = useMemo(
     () =>
